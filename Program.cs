7db35aa8fc1b36d7cd9100e91b2d67537bcae9a4
FlooGeR Alex <fridrih.oushen@yandex.ru>
2021-05-01 21:08:13 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Less08
+{
+    class Program
+    {
+        static void Main()
+        {
+            Type t = typeof(DateTime);
+            foreach (var prop in t.GetProperties())
+                Console.WriteLine(prop.Name);
+            Console.ReadKey();
+        }
+    }
+}
